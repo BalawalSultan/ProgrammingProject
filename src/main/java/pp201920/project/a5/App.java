e9396811da9cfaa86b7e90f4a6c89d0712639a18
Sultan Balawal (Student Com19) <balawal.sultan@stud-inf.unibz.it>
2020-05-26 10:06:14 +0200

    It was the right account so I removed the comment

--- src/main/java/pp201920/project/a5/App.java
+++ src/main/java/pp201920/project/a5/App.java
@@ -6,7 +6,6 @@ import java.util.*;
 import com.google.gson.*;
 
 public class App{
-// random comment
     public static void main(String[] args){
         String json = fetchAndHandle("http://tourism.opendatahub.bz.it/api/Activity");
 
