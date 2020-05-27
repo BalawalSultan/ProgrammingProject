a36bf7fe762230e8d1123e343c83394c862d0dda
Sultan Balawal (Student Com19) <balawal.sultan@stud-inf.unibz.it>
2020-05-27 12:07:05 +0200

    Request.java implements Runnable

--- src/main/java/pp201920/project/a5/Request.java
+++ src/main/java/pp201920/project/a5/Request.java
@@ -1,5 +1,29 @@
 package pp201920.project.a5;
 
-public class Request {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Vector;
+
+public class Request implements Runnable {
+
+    URL url;
+
+    public Request(String url, Vector<String> re){
+        try{
+            this.url = new URL(url);
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+        // try{
+        //     synchronized(response){
+                
+        //     }
+        // }catch(InterruptedException e){
+        //     e.printStackTrace();
+        // }
+    }
     
 }
