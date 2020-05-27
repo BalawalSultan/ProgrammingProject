91e44fce01947b358824fb3c378e6809dc6ff55d
Sultan Balawal (Student Com19) <balawal.sultan@stud-inf.unibz.it>
2020-05-27 12:26:05 +0200

    Now the Request class will fetch data and add it to the vector in Response class

--- src/main/java/pp201920/project/a5/Request.java
+++ src/main/java/pp201920/project/a5/Request.java
@@ -1,29 +1,88 @@
 package pp201920.project.a5;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Vector;
 
 public class Request implements Runnable {
 
     URL url;
+    Response response;
 
-    public Request(String url, Vector<String> re){
+    public Request(String url, Response response){
         try{
             this.url = new URL(url);
         }catch(MalformedURLException e){
             e.printStackTrace();
         }
+
+        this.response = response;
     }
 
     public void run(){
-        // try{
-        //     synchronized(response){
-                
-        //     }
-        // }catch(InterruptedException e){
-        //     e.printStackTrace();
-        // }
+        String result = fetchAndHandle(this.url);
+
+        if(result != null){
+            synchronized(response){
+                response.addResult(result);
+            }
+        }
+    }
+
+    public String fetchAndHandle(URL url){
+        String response = null;
+        
+        try{     
+            response = fetch(url);
+            
+        }catch(NullPointerException e){
+            System.err.println("Empty Response!");
+            e.printStackTrace();
+
+        }catch(FileNotFoundException e){
+            System.err.println("Error 404: FileNotFoundException!");
+            e.printStackTrace();
+
+        }catch(IOException e){
+            System.err.println("IOException!");
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    public String fetch(URL url) throws IOException{        
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+
+        int status = connection.getResponseCode();
+
+        StringBuilder response = new StringBuilder();
+        BufferedReader reader;
+        String line;
+
+        if (status == 404)
+            throw new FileNotFoundException();
+        if (status > 299)
+            reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        else
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        
+        while ((line = reader.readLine()) != null)
+            response.append(line);
+            
+        reader.close();
+
+        if(response.toString().isEmpty())
+            throw new NullPointerException();
+
+        return response.toString();
     }
     
 }
