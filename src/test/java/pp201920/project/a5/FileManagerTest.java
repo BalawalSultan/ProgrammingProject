2d9e20b3320d1c030ab5e0dc3366a0f794707527
PP <bjastkuliar@gmail.com>
2020-06-06 15:24:44 +0200

    Made the FileManagerTest methods more readable

--- src/test/java/pp201920/project/a5/FileManagerTest.java
+++ src/test/java/pp201920/project/a5/FileManagerTest.java
@@ -10,33 +10,44 @@ import org.junit.jupiter.api.Test;
 public class FileManagerTest {
 
     FileManager fileManager = new FileManager();
+    String path,message;
+
 
 
     @Test
     public void getNumOfObjectsTest() throws IOException {
 
-        assertEquals(12,fileManager.readNumOfObjects("src/test/resources/FileManagerTest/TestInput.txt"));
+        path = "src/test/resources/FileManagerTest/TestInput.txt";
+        int result = fileManager.readNumOfObjects(path);
+        assertEquals(12,result,"The file \"TestInput.txt\" should contain the number 12");
 
     }
 
     @Test
     public void getNumOfObjectsDouble() {
 
-        assertThrows(NumberFormatException.class,()->fileManager.readNumOfObjects("src/test/resources/FileManagerTest/TestInputDouble.txt"));
+        path = "src/test/resources/FileManagerTest/TestInputDouble.txt";
+        message = "The file \"TestInputDouble.txt\" should throw a NumberFormatException";
+        assertThrows(NumberFormatException.class,()->fileManager.readNumOfObjects(path),message);
 
     }
 
     @Test
     public void getNumOfObjectsNull() {
 
-        assertThrows(IllegalArgumentException.class,()->fileManager.readNumOfObjects("src/test/resources/FileManagerTest/TestInputNull.txt"));
+        path = "src/test/resources/FileManagerTest/TestInputNull.txt";
+        message = "The file \"TestInputNull.txt\" should throw a IllegalArgumentException";
+        assertThrows(IllegalArgumentException.class,()->fileManager.readNumOfObjects(path),message);
 
     }
 
     @Test
     public void getNumOfObjectsString(){
 
-        assertThrows(NumberFormatException.class,()->fileManager.readNumOfObjects("src/test/resources/FileManagerTest/TestInputString.txt"));
+        path = "src/test/resources/FileManagerTest/TestInputString.txt";
+        message = "The file \"TestInputDouble.txt\" should throw a NumberFormatException";
+        assertThrows(NumberFormatException.class,()->fileManager.readNumOfObjects(path),message);
+
 
     }
 
