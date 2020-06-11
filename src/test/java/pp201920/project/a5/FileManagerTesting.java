ba54a14b15142907153eee19f7ec5a328c3bb028
Alberto <bjastkuliar@gmail.com>
2020-06-11 14:27:08 +0200

    Deleted AnalysisTesting and reduced FileManagerTesting
    Checked that all deleted methods and tests were already implemented

--- src/test/java/pp201920/project/a5/FileManagerTesting.java
+++ src/test/java/pp201920/project/a5/FileManagerTesting.java
@@ -20,42 +20,6 @@ public class FileManagerTesting {
     
 	// //base url linking the actual url to the code for unit testing analysis
 	// String baseUrl= "src/main/java/pp201920/project/a5";
-	
-  
-
-    // public int getNumOfObjects(){
-    	
-    //     int result = 0;
-    //     String path = "src/test/resources/FileManagerTest";
-    //     File inputFile = new File(path + "input.txt");
-
-    //     try{
-    //         FileReader fileReader = new FileReader(inputFile);
-    //         BufferedReader reader = new BufferedReader(fileReader);
-    //         String s = reader.readLine();
-
-    //         if(s != null)
-    //             result = Integer.parseInt(s);
-
-    //         reader.close();
-    //         fileReader.close();
-
-    //     }catch(NumberFormatException e){
-    //         System.err.println("Input.txt does not contain a parsable integer!");
-    //         e.printStackTrace();
-
-    //     }catch(FileNotFoundException e){
-    //         System.err.println("The file \"input.txt\" does not exist or is not in the specified path: " + path);
-    //         e.printStackTrace();
-
-    //     }catch(IOException e) {
-    //         System.err.println("An error occurred while reading the file: input.txt");
-    //         e.printStackTrace();
-    //     }
-
-    //     assertEquals(0, result);
-    //     return result;
-    // }
 
     // public void generateJsonFile(Object object, String fileName){
     //     Gson gson = new GsonBuilder().serializeNulls().disableHtmlEscaping().setPrettyPrinting().create();
